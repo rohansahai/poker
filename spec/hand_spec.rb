fbd2b2da39cefd438e61284a82eba4daa69e494d
Rohan Sahai <rsahai91@gmail.com>
2014-04-11 16:39:13 -0700

    Logic for winning hands written

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -14,6 +14,87 @@ describe Hand do
   ]) }
   let(:bad_hand)   { Hand.new(bad_deck)}
 
+  let(:royal_cards) { Deck.new([
+    Card.new(:spade, :ten),
+    Card.new(:spade, :jack),
+    Card.new(:spade, :queen),
+    Card.new(:spade, :king),
+    Card.new(:spade, :ace)
+  ]) }
+  let(:royal_hand) { Hand.new(royal_cards) }
+
+  let(:straight_deck) { Deck.new([
+    Card.new(:spade, :ten),
+    Card.new(:heart, :jack),
+    Card.new(:spade, :queen),
+    Card.new(:club, :king),
+    Card.new(:spade, :nine)
+  ]) }
+  let(:straight_hand) { Hand.new(straight_deck) }
+
+  let(:flush_deck) { Deck.new([
+    Card.new(:spade, :ace),
+    Card.new(:spade, :jack),
+    Card.new(:spade, :seven),
+    Card.new(:spade, :deuce),
+    Card.new(:spade, :nine)
+  ]) }
+  let(:flush_hand) { Hand.new(flush_deck) }
+
+  let(:straight_flush_deck) { Deck.new([
+    Card.new(:spade, :deuce),
+    Card.new(:spade, :three),
+    Card.new(:spade, :four),
+    Card.new(:spade, :five),
+    Card.new(:spade, :six)
+  ]) }
+  let(:straight_flush_hand) { Hand.new(straight_flush_deck) }
+
+  let(:three_deck) { Deck.new([
+    Card.new(:heart, :four),
+    Card.new(:club, :three),
+    Card.new(:diamond, :four),
+    Card.new(:spade, :four),
+    Card.new(:spade, :six)
+  ]) }
+  let(:three_hand) { Hand.new(three_deck) }
+
+  let(:four_deck) { Deck.new([
+    Card.new(:heart, :four),
+    Card.new(:club, :four),
+    Card.new(:diamond, :four),
+    Card.new(:spade, :four),
+    Card.new(:spade, :six)
+  ]) }
+  let(:four_hand) { Hand.new(four_deck) }
+
+  let(:pair_deck) { Deck.new([
+    Card.new(:heart, :four),
+    Card.new(:club, :three),
+    Card.new(:diamond, :jack),
+    Card.new(:spade, :four),
+    Card.new(:spade, :six)
+  ]) }
+  let(:pair_hand) { Hand.new(pair_deck) }
+
+  let(:two_pair_deck) { Deck.new([
+    Card.new(:heart, :four),
+    Card.new(:club, :three),
+    Card.new(:diamond, :six),
+    Card.new(:spade, :four),
+    Card.new(:spade, :six)
+  ]) }
+  let(:two_pair_hand) { Hand.new(two_pair_deck) }
+
+  let(:full_house_deck) { Deck.new([
+    Card.new(:heart, :six),
+    Card.new(:club, :six),
+    Card.new(:diamond, :jack),
+    Card.new(:spade, :jack),
+    Card.new(:spade, :six)
+  ]) }
+  let(:full_house_hand) { Hand.new(full_house_deck) }
+
   describe "#initialize" do
     it "should have five cards" do
       expect(hand.count).to eq(5)
@@ -40,15 +121,6 @@ describe Hand do
   end
 
   describe "#royal_flush?" do
-    let(:royal_cards) { Deck.new([
-      Card.new(:spade, :ten),
-      Card.new(:spade, :jack),
-      Card.new(:spade, :queen),
-      Card.new(:spade, :king),
-      Card.new(:spade, :ace)
-    ]) }
-    let(:royal_hand) { Hand.new(royal_cards) }
-
     it "should return true if royal flush" do
       expect(royal_hand).to be_royal_flush
     end
@@ -59,16 +131,6 @@ describe Hand do
   end
 
   describe "#straight?" do
-    let(:straight_deck) { Deck.new([
-      Card.new(:spade, :ten),
-      Card.new(:heart, :jack),
-      Card.new(:spade, :queen),
-      Card.new(:club, :king),
-      Card.new(:spade, :nine)
-    ]) }
-
-    let(:straight_hand) { Hand.new(straight_deck) }
-
     it "should return true if hand contains straight" do
       expect(straight_hand).to be_straight
     end
@@ -80,16 +142,6 @@ describe Hand do
   end
 
   describe "#flush?" do
-    let(:flush_deck) { Deck.new([
-      Card.new(:spade, :ace),
-      Card.new(:spade, :jack),
-      Card.new(:spade, :seven),
-      Card.new(:spade, :deuce),
-      Card.new(:spade, :nine)
-    ]) }
-
-    let(:flush_hand) { Hand.new(flush_deck) }
-
     it "should return true if hand contains flush" do
       expect(flush_hand).to be_flush
     end
@@ -100,16 +152,6 @@ describe Hand do
   end
 
   describe "#straight_flush?" do
-    let(:straight_flush_deck) { Deck.new([
-      Card.new(:spade, :deuce),
-      Card.new(:spade, :three),
-      Card.new(:spade, :four),
-      Card.new(:spade, :five),
-      Card.new(:spade, :six)
-    ]) }
-
-    let(:straight_flush_hand) { Hand.new(straight_flush_deck) }
-
     it "should return true if hand contains straight flush" do
       expect(straight_flush_hand).to be_straight_flush
     end
@@ -120,16 +162,6 @@ describe Hand do
   end
 
   describe "#three_of_a_kind?" do
-    let(:three_deck) { Deck.new([
-      Card.new(:heart, :four),
-      Card.new(:club, :three),
-      Card.new(:diamond, :four),
-      Card.new(:spade, :four),
-      Card.new(:spade, :six)
-    ]) }
-
-    let(:three_hand) { Hand.new(three_deck) }
-
     it "should return true if hand contains three of a kind" do
       expect(three_hand).to be_three_of_a_kind
     end
@@ -140,16 +172,6 @@ describe Hand do
   end
 
   describe "#four_of_a_kind?" do
-    let(:four_deck) { Deck.new([
-      Card.new(:heart, :four),
-      Card.new(:club, :four),
-      Card.new(:diamond, :four),
-      Card.new(:spade, :four),
-      Card.new(:spade, :six)
-    ]) }
-
-    let(:four_hand) { Hand.new(four_deck) }
-
     it "should return true if hand contains four of a kind" do
       expect(four_hand).to be_four_of_a_kind
     end
@@ -160,16 +182,6 @@ describe Hand do
   end
 
   describe "#pair?" do
-    let(:pair_deck) { Deck.new([
-      Card.new(:heart, :four),
-      Card.new(:club, :three),
-      Card.new(:diamond, :jack),
-      Card.new(:spade, :four),
-      Card.new(:spade, :six)
-    ]) }
-
-    let(:pair_hand) { Hand.new(pair_deck) }
-
     it "should return true if hand contains pair" do
       expect(pair_hand).to be_pair
     end
@@ -180,16 +192,6 @@ describe Hand do
   end
 
   describe "#two_pair?" do
-    let(:two_pair_deck) { Deck.new([
-      Card.new(:heart, :four),
-      Card.new(:club, :three),
-      Card.new(:diamond, :six),
-      Card.new(:spade, :four),
-      Card.new(:spade, :six)
-    ]) }
-
-    let(:two_pair_hand) { Hand.new(two_pair_deck) }
-
     it "should return true if hand contains two_pair" do
       expect(two_pair_hand).to be_two_pair
     end
@@ -200,16 +202,6 @@ describe Hand do
   end
 
   describe "#full_house?" do
-    let(:full_house_deck) { Deck.new([
-      Card.new(:heart, :six),
-      Card.new(:club, :six),
-      Card.new(:diamond, :jack),
-      Card.new(:spade, :jack),
-      Card.new(:spade, :six)
-    ]) }
-
-    let(:full_house_hand) { Hand.new(full_house_deck) }
-
     it "should return true if hand contains full_house" do
       expect(full_house_hand).to be_full_house
     end
